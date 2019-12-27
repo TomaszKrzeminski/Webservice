74691e2e14cb4885d1319955a26262377408dd9a
Tomasz Krzeminski <koral2323@gmail.com>
2019-12-27 16:41:43 +0100

    Final

---
+++ WebServiceApp/Controllers/CalculatorSoapController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Calculator;
+using static Calculator.CalculatorSoapClient;
+using WebServiceApp.Models;
+
+namespace WebServiceApp.Controllers
+{
+    public class CalculatorSoapController : Controller
+    {
+
+        public IActionResult Calculator()
+        {
+            CalculatorData data = new CalculatorData() {A=10,B=5,Operation="Add" };
+            return View("CalculatorView",data);
+        }
+
+
+
+
+
+        public async Task<IActionResult> Action(CalculatorData data)
+        {
+
+            var calc = new CalculatorSoapClient(EndpointConfiguration.CalculatorSoap);
+
+
+
+            switch (data.Operation)
+            {
+                case "Add":
+                   data.Result = await calc.AddAsync(data.A, data.B);
+                    break;
+                case "Divide":
+                    data.Result = await calc.DivideAsync(data.A, data.B);
+                    break;
+                case "Subtract":
+                    data.Result = await calc.SubtractAsync(data.A, data.B);
+                    break;
+                case "Multiply":
+                    data.Result = await calc.MultiplyAsync(data.A, data.B);
+                    break;
+                default:
+                   
+                    break;
+            }
+
+
+
+
+
+
+            return View("CalculatorResult", data);
+        }
+
+
+
+
+        public async Task<IActionResult> Add()
+        {
+
+            var calc = new CalculatorSoapClient(EndpointConfiguration.CalculatorSoap);
+
+            int add =await calc.AddAsync(1, 2);
+
+            return View(add);
+        }
+        public async Task<IActionResult> Divide()
+        {
+            var calc = new CalculatorSoapClient(EndpointConfiguration.CalculatorSoap);
+
+            int add = await calc.DivideAsync(4, 2);
+
+            return View(add);
+        }
+        public async Task<IActionResult> Subtract()
+        {
+            var calc = new CalculatorSoapClient(EndpointConfiguration.CalculatorSoap);
+
+            int add = await calc.SubtractAsync(22, 2);
+
+            return View(add);
+        }
+        public async Task<IActionResult> Multiply()
+        {
+            var calc = new CalculatorSoapClient(EndpointConfiguration.CalculatorSoap);
+
+            int add = await calc.MultiplyAsync(3, 4);
+
+            return View(add);
+        }
+    }
+}
---
+++ WebServiceApp/Models/CalculatorData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServiceApp.Models
+{
+    public class CalculatorData
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Result { get; set; }
+        public string Operation { get; set; }
+    }
+}
